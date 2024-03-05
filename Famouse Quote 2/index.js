bff241805d8fe6d9c118bcc1945e6d478e2bbdd2
Ahmed Raza Abbasi <161449719+AhmedrazA2772@users.noreply.github.com>
2024-03-05 21:52:18 +0500

    famousQuote2

---
+++ Famouse Quote 2/index.js
@@ -0,0 +1,5 @@
+// famousQuote2.ts
+var famousPerson = "Albert Einstein";
+var quote = "A person who never made a mistake never tried anything new.";
+var message = "".concat(famousPerson, " once said, \"").concat(quote, "\"");
+console.log(message);
---
+++ Famouse Quote 2/index.ts
@@ -0,0 +1,7 @@
+// famousQuote2.ts
+
+const famousPerson: string = "Albert Einstein";
+const quote: string = "A person who never made a mistake never tried anything new.";
+const message: string = `${famousPerson} once said, "${quote}"`;
+
+console.log(message);
