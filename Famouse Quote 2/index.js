63d8a0870465e22d0bb6fd1f2af8587f8d3473bb
Ahmed Raza Abbasi <161449719+AhmedrazA2772@users.noreply.github.com>
2024-03-05 22:03:54 +0500

    Update index.js

--- Famouse Quote 2/index.js
+++ Famouse Quote 2/index.js
@@ -2,3 +2,6 @@ console.log
 var famousPerson = "Albert Einstein";
 var quote = "A person who never made a mistake never tried anything new.";
 var message = "".concat(famousPerson, " once said, \"").concat(quote, "\"");
+
+console.log("FamousePerson");
+console.log("A person who never made a mistake never tried anything new")
